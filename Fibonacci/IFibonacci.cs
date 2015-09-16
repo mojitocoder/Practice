a8f7929c4cdd02ec7bf27b6142cf1ab425b09500
Quynh Nguyen <quynhnh@gmail.com>
2015-09-16 11:50:16 +0100

    Updated

--- Fibonacci/IFibonacci.cs
+++ Fibonacci/IFibonacci.cs
@@ -52,4 +52,6 @@ namespace Fibonacci
             return fibs;
         }
     }
+
+
 }
---
+++ Fibonacci/Singleton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    public sealed class Singleton
+    {
+        private Singleton() { }
+
+        private static readonly Lazy<Singleton> lazy = new Lazy<Singleton>(() => new Singleton());
+
+        public static Singleton Instance
+        {
+            get
+            {
+                return lazy.Value;
+            }
+        }
+    }
+
+    public sealed class LazySingleton
+    {
+        private LazySingleton()
+        { }
+
+        private static readonly Lazy<LazySingleton> lazy = new Lazy<LazySingleton>(() => new LazySingleton());
+
+        public static LazySingleton Instance
+        {
+            get
+            {
+                return lazy.Value;
+            }
+        }
+
+    }
+}
